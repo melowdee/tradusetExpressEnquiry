153465ff92428c3d6df51650d60fe3efb5ee5b2d
Dirk Müller <mueller@kabelwelten.de>
2014-10-05 14:32:55 +0200

    namen decoden

--- tradusetExpressEnquiry.php
+++ tradusetExpressEnquiry.php
@@ -167,7 +167,7 @@ beglaubigt: %s
 ---
 Diese E-Mail wurde über das Expressformular von traduset.de gesendet";
 
-                    $message = sprintf($format, urldecode($_POST['customerName']), $_POST['customerEmail'], $_POST['sourceLanguage'], $_POST['targetLanguage'], $_POST['certified']);
+                    $message = sprintf($format, htmlentities($_POST['customerName'],null,"UTF-8"), $_POST['customerEmail'], $_POST['sourceLanguage'], $_POST['targetLanguage'], $_POST['certified']);
 
                     if (!filter_var($enquiryEmail, FILTER_VALIDATE_EMAIL))
                         $enquiryEmail = 'info@traduset.de';
